759d06128f3cebd784658fc2ca8e9d91eb5ece94
GeeckoDev <geecko.dev@free.fr>
2012-07-02 11:43:57 +0200

    corrected sample build

--- sample/clock/main.c
+++ sample/clock/main.c
@@ -3,25 +3,10 @@
 #include <pspkernel.h>
 #include <psprtc.h>
 #include "../../glib2d.h"
+#include "../callbacks.h"
 
 PSP_MODULE_INFO("App",0,1,1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
-
-/* Callbacks */
-int exit_callback(int arg1, int arg2, void *common) {
-  sceKernelExitGame();
-  return 0; }
-int CallbackThread(SceSize args, void *argp) {
-  int cbid;
-  cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-  sceKernelRegisterExitCallback(cbid);
-  sceKernelSleepThreadCB();
-  return 0; }
-int SetupCallbacks() {
-  int thid = 0;
-  thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
-  if(thid >= 0) sceKernelStartThread(thid, 0, 0);
-  return thid; }
 
 pspTime time;
 
@@ -93,7 +78,7 @@ void drawClockHands()
 
 int main()
 {
-  SetupCallbacks();
+  callbacks_setup();
 
   while (1)
   {
--- sample/objcontrol/main.c
+++ sample/objcontrol/main.c
@@ -3,32 +3,17 @@
 #include <pspkernel.h>
 #include <pspctrl.h>
 #include "../../glib2d.h"
+#include "../callbacks.h"
 
 PSP_MODULE_INFO("App",0,1,1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
-/* Callbacks */
-int exit_callback(int arg1, int arg2, void *common) {
-  sceKernelExitGame();
-  return 0; }
-int CallbackThread(SceSize args, void *argp) {
-  int cbid;
-  cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-  sceKernelRegisterExitCallback(cbid);
-  sceKernelSleepThreadCB();
-  return 0; }
-int SetupCallbacks() {
-  int thid = 0;
-  thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
-  if(thid >= 0) sceKernelStartThread(thid, 0, 0);
-  return thid; }
-
 int main()
 {
-  SetupCallbacks();
+  callbacks_setup();
 
   SceCtrlData pad;
-  g2dImage* tex = g2dTexLoad("tex.png",G2D_SWIZZLE);
+  g2dTexture* tex = g2dTexLoad("tex.png",G2D_SWIZZLE);
   int alpha = 255,
       x = G2D_SCR_W/2,
       y = G2D_SCR_H/2,
